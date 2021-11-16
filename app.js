6128c2df1f9ed5f89dacf8dfc91ceab7e85cfa5e
Mortiza Nourani <mortizanourani@gmail.com>
2021-11-16 23:46:13 +0330

    The passport module installed. Passport authentication middleware added in register and login methods.

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 var indexRouter = require('./routes/index');
 var accountRouter = require('./routes/account');
-const session = require('express-session');
 
 var app = express();
 
@@ -14,6 +17,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: 'crystal it',
@@ -21,14 +29,53 @@ app.use(
     resave: true,
   })
 );
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/account', accountRouter);
+
+const Context = require('./models/context');
+const Account = require('./models/account');
+const md5 = require('md5');
+
+passport.use(
+  'register',
+  new LocalStrategy(function (username, password, done) {
+    let account = new Account();
+    account.username = username;
+    account.passwordHash = md5(password);
+    Context.Account.insertOne(account, function (err) {
+      if (err) return done(err);
+      return done(null, true);
+    });
+  })
+);
+
+passport.use(
+  'login',
+  new LocalStrategy(function (username, password, done) {
+    Context.Account.findOne(
+      {
+        username: username,
+        passwordHash: md5(password),
+      },
+      function (err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect username.' });
+        return done(null, user);
+      }
+    );
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/account.js
+++ routes/account.js
@@ -3,61 +3,48 @@ var router = express.Router();
 
 const Context = require('../models/context');
 const Account = require('../models/account');
+const passport = require('passport');
+
+/* GET Account page. */
+router.get('/', function (req, res, next) {
+  if (req.isAuthenticated())
+    return res.redirect('/dashboard/');
+  
+  res.redirect('/account/login/');
+});
 
 /* GET Register page. */
 router.get('/register', function (req, res, next) {
+  if (req.isAuthenticated())
+    return res.redirect('/dashboard/');
+  
   res.render('account/register', { title: 'CrystalIT | Register' });
 });
 
 /* POST Register proccess */
-router.post('/register', async function (req, res, next) {
-  let account = new Account();
-  account.username = req.body.username;
-  account.passwordHash = req.body.password;
-
-  if (await account.isTaken())
-    return res.send('یک حساب کاربری با این نام کاربری موجود است.');
-
-  if (!account.isPasswordValid())
-    return res.send('رمزعبور وارد شده نامناسب است.');
-
-  account.create();
-
-  let url = '/account/login/';
-  res.redirect(url);
+router.post('/register',
+  passport.authenticate('register', { failureRedirect: '/account/register/' }),
+  function (req, res, next) {
+    res.redirect('/account/login/');
 });
 
 /* GET Login page. */
 router.get('/login', function (req, res, next) {
-  let session = req.session;
-  let errorMessage = session.message;
+  if (req.isAuthenticated())
+    return res.redirect('/dashboard/');
+  
   res.render('account/login', {
     title: 'CrystalIT | Login',
-    message: errorMessage,
   });
 });
 
 /* POST Login page. */
-router.post('/login', async function (req, res, next) {
-  let session = req.session;
-
-  let username = req.body.username;
-  let password = req.body.password;
-
-  let account = new Account();
-  Object.assign(account, await Context.Account.findOne({ username: username }));
-  
-  if (account._id === undefined || !account.isPasswordCorrect(password)) {
-    let errorMessage = 'نام کاربری یا رمز عبور نادرست می باشد.';
-    res.render('account/login', {
-      title: 'CrystalIT | Login',
-      message: errorMessage,
-    });
-  } else {
-    delete account.passwordHash;
-    session.Account = account;
+router.post(
+  '/login',
+  passport.authenticate('login', { failureRedirect: '/account/login/' }),
+  function (req, res, next) {
     res.redirect('/dashboard/');
   }
-});
+);
 
 module.exports = router;
