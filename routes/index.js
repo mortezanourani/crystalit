d5b0435f56f7a80649bd163e7a006ee6f3f52aaf
Mortiza Nourani <mortizanourani@gmail.com>
2021-11-29 00:51:10 +0330

    Add to cart added. There is a AddToCart link on the product page. It puts the product to Session, in cart property. The cart property is an array includes products as items.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const Product = require('../models/product');
+const Item = require('../models/item');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -30,10 +31,33 @@ router.get('/product/:id', async function (req, res, next) {
   let product = new Product();
   product._id = req.params.id;
   await product.find();
+
+  let cart = req.session.cart;
+  console.log(cart);
+  if (!cart)
+    cart = [];
   res.render('product', {
     title: 'CrystalIT | Shop',
     product: product,
+    cart: cart,
   });
+});
+
+/* GET add a product to cart */
+router.get('/cart/add/:id', async function (req, res, next) {
+  let product = new Product();
+  product._id = req.params.id;
+  await product.find();
+
+  let item = new Item(product);
+
+  let cart = req.session.cart;
+  if (typeof cart !== 'object')
+    cart = [];
+  cart.push(item);
+  req.session.cart = cart;
+  
+  return res.redirect('/product/' + product._id);
 });
 
 module.exports = router;
